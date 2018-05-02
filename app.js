00e85e79e135c9a035103a52f786cda5a66484ad
DaiKunz <alexandra.d.kunz@gmail.com>
2018-05-02 17:09:52 -0600

    made a blog

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $.ajax({
+        url: "http://faker.hook.io/?property=name.firstName",
+        method: 'GET',
+        dataType: 'JSON'
+    }).done( function(firstName) {
+        setName(firstName)
+    })
+
+    $.ajax({
+        url: "http://faker.hook.io/?property=date.recent",
+        method: 'GET',
+        dataType: 'JSON'
+    }).done( function(date) {
+        setDate(date)
+    })
+
+    function setName(name) {
+        $('#name').html(name)
+    }
+
+    function setPic(img) {
+        $('#profile-pic').attr("src", img)
+    }
+
+    function setDate(date) {
+        var day = moment(date).date()
+        var month = moment(date).format('MMMM')
+        var year = moment(date).year()
+        $('#date').html(month + "" + day + "," + "" + year)
+    }
+});
